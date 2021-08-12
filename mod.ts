6bfe452281809d894b68bd44d895f09cae41fd02
Jesús Otero <jesusoterogomez@gmail.com>
2021-08-12 13:08:12 +0200

    http

--- mod.ts
+++ mod.ts
@@ -1,1 +1,13 @@
 console.log("Hello Penneo");
+
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    new Response("Hello Penneo", {
+      status: 200,
+      headers: {
+        server: "deploy",
+        "content-type": "text/plain",
+      },
+    })
+  );
+});
